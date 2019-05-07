b1efa679177e45ff27ef138309e48dc6af29a43e
FernandoIgorrr <FernandoIgor.Dantas@outlook.com>
2019-05-07 05:13:26 -0300

    Projeto finalizado, Agr documentado (apenas escrito), falta só executar o doxygen

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+/**
+* @file main.cpp
+* @version 1.0
+* @date 06/05/2019
+* @author Fernando Igor Dantas
+* @title Small Program in C++
+* @brief for college
+* @code
+* */
+
 #include <iostream>
 #include <string>
 #include <cstring>
@@ -7,31 +17,23 @@
 
 using namespace std;
 
-
-/**
-* @brief Função main
-* @author Fernando Iogr Dantas
-*
-* @since 06-05-2019
-* */
-
 int main(){
 
-	string command = "man";	
-	int i = command.compare("quit");
+     string command = "man";  
+     int i = command.compare("quit");
 
-	/**
-	*@brief loop que mantém o prorama funcionando até digitar quit ou ele crashar
-	* */
-	while(i){
-	
+     /**
+     *@brief loop que mantém o prorama funcionando até digitar quit ou ele crashar
+     * */
+     while(i){
+     
         setbuf(stdin, NULL);
-		cout << "->";	
-		getline(cin,command);
-		Command *c = new Command(command);
-		c->getCommand();
-		i   = command.compare("quit");
-		
-	}
-	return 0;
+          cout << "->";  
+          getline(cin,command);
+          Command *c = new Command(command);
+          c->getCommand();
+          i   = command.compare("quit");
+          
+     }
+     return 0;
 }
